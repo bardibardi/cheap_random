e2e6c56952eefd0a9f584bd115a362f33228401d
Bardi Einarsson <bardi_e@hotmail.com>
2012-06-04 18:26:31 -0700

    examples/cr.rb

--- examples/cr.rb
+++ examples/cr.rb
@@ -1,11 +1,11 @@
 CHEAP_DEPENDENCY_ENV_NAME = 'CR'
-load "#{File.dirname(__FILE__)}/cheap_dependency.rb"
+load File.expand_path('../lib/cheap_dependency.rb', File.dirname(__FILE__))
 CheapDependency.cd_get(
   'cheap_random',
   'cheap_file',
-  'cheap_big_file',
-  'cheap_file_rc'
+  'cheap_big_file'
 )
+load File.expand_path('cheap_file_rc.rb', File.dirname(__FILE__))
 
 if CheapDependency.cd_should_load?
   CheapDependency.cd_get 'cheap_test'
