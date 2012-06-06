3c99b74b732881fdf86b9079a4a6960718a96604
Bardi Einarsson <bardi_e@hotmail.com>
2012-06-06 16:18:26 -0700

    chi_squared example

---
+++ examples/chi_squared.rb
@@ -0,0 +1,37 @@
+p File.absolute_path(__FILE__)
+CHEAP_DEPENDENCY_ENV_NAME = 'CD'
+load File.expand_path('../lib/cheap_dependency.rb', File.dirname(__FILE__))
+CheapDependency.cd_get('cheap_byte_count')
+
+BASE_DIR = File.expand_path('../../random', File.dirname(__FILE__))
+XLAT_EXT = '.random'
+
+def byte_count_array(file_name)
+  afn = "#{BASE_DIR}/#{file_name}#{XLAT_EXT}"
+  CheapByteCount.byte_count_array_from_file afn
+end
+
+def chi(a)
+  d = a.length
+  n = a.reduce(:+)
+  p n
+  e = (n*1.0)/d
+  p e
+  c = a.reduce(0) {|acc, r| acc + (r - e)*(r - e)/e}
+  [d - 1, c]
+end
+
+def chi_of_bytes(file_name)
+  bca = byte_count_array file_name
+  chi bca
+end
+
+def l
+  load File.absolute_path(__FILE__)
+end
+
+if !CheapDependency.cd_test?
+  file_name = ARGV[0]
+  p chi_of_bytes(file_name)
+end
+
